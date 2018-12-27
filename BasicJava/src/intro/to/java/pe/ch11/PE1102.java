3b8172c604194eeec666f0874ff9f6828f39991c
Evan <evanyang.developer@gmail.com>
2018-12-27 22:29:03 +0800

    Delete from Windows 10

--- BasicJava/src/intro/to/java/pe/ch11/PE1102.java
+++
@@ -1,8 +0,0 @@
-package intro.to.java.pe.ch11;
-
-public class PE1102 {
-	public static void main(String[] args) {
-		Staff staff = new Staff("KFC", "Happy Valley" ,"intern");
-		System.out.println(staff.toString());
-	}
-}
