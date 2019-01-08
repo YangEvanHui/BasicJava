3f7d9d8afd3f10ab00ba002048d291e7ab363bbe
Evan <evanyang.developer@gmail.com>
2019-01-08 16:20:18 +0800

    Commit from Windows 10

---
+++ BasicJava/src/intro/to/java/ch12/WebCrawler.java
@@ -0,0 +1,68 @@
+package intro.to.java.ch12;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class WebCrawler {
+	public static ArrayList<String> getSubURLs(String urlString) {
+		ArrayList<String> list = new ArrayList<>();
+		
+		try {
+			URL url = new URL(urlString);
+			Scanner input = new Scanner(url.openStream());
+			int current = 0;
+			while (input.hasNext()) {
+				String line = input.nextLine();
+				current = line.indexOf("https:", current);
+				while (current > 0) {
+					int endIndex = line.indexOf("\"", current);
+					
+					// Ensure that a correct URL is found
+					if (endIndex > 0) {
+						current = line.indexOf("https:", endIndex);
+					} else {
+						current = -1;
+					}
+				}
+			}
+			input.close();
+		} catch (Exception e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		
+		return list;
+	}
+	
+	public static void crawler(String startingURL) {
+		ArrayList<String> listOfPendingURLs = new ArrayList<>();
+		ArrayList<String> listOfTraversedURLs = new ArrayList<>();
+		
+		listOfPendingURLs.add(startingURL);
+		while (!listOfPendingURLs.isEmpty() &&
+				listOfTraversedURLs.size() <= 100) {
+			String urlString = listOfPendingURLs.remove(0);
+			if (!listOfTraversedURLs.contains(urlString)) {
+				listOfTraversedURLs.add(urlString);
+				System.out.println("Craw " + urlString);
+				
+				for (String s : getSubURLs(urlString)) {
+					if (!listOfTraversedURLs.contains(s)) {
+						listOfPendingURLs.add(s);
+					}
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a URL: ");
+		String url = input.nextLine();
+		
+		// Traverse the Web from the starting url
+		crawler(url);
+		
+		input.close();
+	}
+}
